d747b12a8ace68f70517b952367f57a8bf960dc8
rajan <rajan@LAPTOP-BLO4TQ23.localdomain>
2023-05-25 06:20:03 +0530

    provider init

---
+++ hashicups/provider.go
@@ -0,0 +1,47 @@
+package hashicups
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/provider"
+	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+)
+
+// Ensure the implementation satisfies the expected interfaces
+var (
+	_ provider.Provider = &hashicupsProvider{}
+)
+
+// New is a helper function to simplify provider server and testing implementation.
+func New() provider.Provider {
+	return &hashicupsProvider{}
+}
+
+// hashicupsProvider is the provider implementation.
+type hashicupsProvider struct{}
+
+// Metadata returns the provider type name.
+func (p *hashicupsProvider) Metadata(_ context.Context, _ provider.MetadataRequest, resp *provider.MetadataResponse) {
+	resp.TypeName = "hashicups"
+}
+
+// Schema defines the provider-level schema for configuration data.
+func (p *hashicupsProvider) Schema(_ context.Context, _ provider.SchemaRequest, resp *provider.SchemaResponse) {
+	resp.Schema = schema.Schema{}
+}
+
+// Configure prepares a HashiCups API client for data sources and resources.
+func (p *hashicupsProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+}
+
+// DataSources defines the data sources implemented in the provider.
+func (p *hashicupsProvider) DataSources(_ context.Context) []func() datasource.DataSource {
+	return nil
+}
+
+// Resources defines the resources implemented in the provider.
+func (p *hashicupsProvider) Resources(_ context.Context) []func() resource.Resource {
+	return nil
+}
